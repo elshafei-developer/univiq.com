b6039817e984edbe965eeec85ba789a0ccd7111d
El-Shafei developer <el.shafei.developer@gmail.com>
2023-06-13 19:52:29 +0300

    finish 1.0

--- js/advancedSearch.js
+++ js/advancedSearch.js
@@ -1,5 +1,15 @@
 "use strict";
-// Cities Iraq
+let btnFindUnivAverage = document.querySelector("#btnFindUnivAverage");
+let btnFindUnivFilter = document.querySelector("#btnFindUnivFilter");
+let tbody = document.querySelector("tbody");
+let countResult = document.querySelector("#countResult");
+let universityFilter = document.querySelector("#universityFilter");
+let collegeFilter = document.querySelector("#collegeFilter");
+let yearFilter = document.querySelector("#yearFilter");
+let yearAverage = document.querySelector("#year");
+let cityFilter = document.querySelector("#cityFilter");
+
+// Object Cities Iraq
 const cities = {
   baghdad: [
     "جامعة بغداد",
@@ -32,7 +42,7 @@ const cities = {
   ],
   diyala: ["جامعة ديالى", "الجامعة التقنية الوسطى"],
   karbala: ["جامعة كربلاء", "جامعة الفرات الاوسط التقنية "],
-  dhiQar: ["جامعة ذي قار", "جامعة سومر", "الجامعة التقنية الجنوبية"],
+  dhiqar: ["جامعة ذي قار", "جامعة سومر", "الجامعة التقنية الجنوبية"],
   kirkuk: ["جامعة كركوك", "الجامعة التقنية الشمالية"],
   wasit: ["جامعة واسط", "الجامعة التقنية الوسطى"],
   maysan: ["جامعة ميسان", "الجامعة التقنية الجنوبية"],
@@ -45,61 +55,133 @@ const cities = {
     "الجامعة التقنية الشمالية",
   ],
 };
-// ******************************** \\
+
 // Start
+let year = document.querySelector("#year").value;
+addUniversity(filterYear(year)).then(addCollege);
+
+btnFindUnivAverage.addEventListener("click", ShowResultByAverage);
+btnFindUnivFilter.addEventListener("click", ShowResultByFilter);
+
+yearFilter.addEventListener("change", changeYear);
+yearAverage.addEventListener("change", changeYear);
+cityFilter.addEventListener("change", changeCities);
+
+// ONLY Functions ONLY Functions
+// Fetch All Data
 async function getData(link) {
   try {
     let getData = await fetch(link);
     let getJSON = await getData.json();
     return getJSON;
   } catch {
-    return "Not Found Data";
+    return new Error("Not Found Data");
   }
 }
-let btnFindUnivAverage = document.querySelector("#btnFindUnivAverage");
-let tbody = document.querySelector("tbody");
-let countResult = document.querySelector("#countResult");
 
-btnFindUnivAverage.addEventListener("click", ShowResultByAverage);
+function filterYear(year) {
+  return getData(`./DB/DB${year}.json`);
+}
 
 async function ShowResultByAverage() {
   let year = document.querySelector("#year").value;
   let citiesSelect = document.querySelector("#cityAverage option:checked");
-  await getData(`./DB/DB${year}.json`)
-    .then((univ) => {
-      return filterCities(univ, citiesSelect);
+  filterYear(year)
+    .then((cities) => {
+      return filterCities(cities, citiesSelect);
     })
     .then((branch) => {
-      return filterBranch(branch, "branch");
+      return filterBranchAndGender(branch, "branch", "branch");
     })
     .then((gender) => {
-      return filterBranch(gender, "gender");
+      return filterBranchAndGender(gender, "gender", "gender");
     })
     .then((average) => {
-      let averageSelect = document.querySelector(`#average`);
-      let arrAverage = [];
-      console.log(typeof averageSelect.value);
-      if (+averageSelect.value > 50 && +averageSelect.value < 106) {
-        average.forEach((el) => {
-          if (el.average <= +averageSelect.value) {
-            arrAverage.push(el);
-          }
-        });
-        return arrAverage;
-      } else if (+averageSelect.value == 0) {
-        console.log(+averageSelect.value);
-        return average;
-      } else if (+averageSelect.value < 50 || +averageSelect.value > 105) {
-        alert("ادخل معدل صحيح");
-        // return average;
-      }
+      return filterAverage(average);
     })
     .then((final) => {
       return appendResult(final, citiesSelect);
     });
 }
 
-// Function ONLY
+async function ShowResultByFilter() {
+  let year = document.querySelector("#yearFilter").value;
+  let citiesSelect = document.querySelector("#cityFilter option:checked");
+  filterYear(year)
+    .then((cities) => {
+      return filterCities(cities, citiesSelect);
+    })
+    .then((theData) => {
+      return filterUniversityAndCollege(theData, "universityFilter", 0);
+    })
+    .then((theData) => {
+      return filterUniversityAndCollege(theData, "collegeFilter", 1);
+    })
+    .then((branch) => {
+      return filterBranchAndGender(branch, "branchFilter", "branch");
+    })
+    .then((gender) => {
+      return filterBranchAndGender(gender, "genderFilter", "gender");
+    })
+    .then((final) => {
+      return appendResult(final, citiesSelect);
+    });
+}
+
+function changeYear(event) {
+  let year = event.target.value;
+  addUniversity(filterYear(year)).then(addCollege);
+}
+
+async function changeCities(event) {
+  let citiesSelect = event.target;
+  let data = await filterYear(year);
+  let setData = filterCities(data, citiesSelect);
+  addUniversity(setData).then(addCollege);
+}
+
+async function addUniversity(data) {
+  let allOption = document.querySelectorAll("#universityFilter option");
+  if (allOption.length > 1) {
+    universityFilter.textContent = "";
+    let option = document.createElement("option");
+    option.textContent = "كل الجامعات";
+    option.setAttribute("value", "all");
+    universityFilter.appendChild(option);
+  }
+  let getDataOption = await data;
+  let setOptionUniv = new Set();
+  getDataOption.forEach((el) => {
+    setOptionUniv.add(el.university.split("/")[0]);
+  });
+  setOptionUniv.forEach((el) => {
+    let option = document.createElement("option");
+    option.textContent = el;
+    universityFilter.appendChild(option);
+  });
+  return data;
+}
+
+function addCollege(data) {
+  let allOption = document.querySelectorAll("#collegeFilter option");
+  if (allOption.length >= 1) {
+    collegeFilter.textContent = "";
+    let option = document.createElement("option");
+    option.textContent = "الكل";
+    option.setAttribute("value", "all");
+    collegeFilter.appendChild(option);
+  }
+  let setOptionUniv = new Set();
+  data.forEach((el) => {
+    setOptionUniv.add(el.university.split("/")[1]);
+  });
+  setOptionUniv.forEach((el) => {
+    let option = document.createElement("option");
+    option.textContent = el;
+    collegeFilter.appendChild(option);
+  });
+}
+
 function filterCities(univ, citiesSelect) {
   if (citiesSelect.value === "all") {
     return univ;
@@ -116,18 +198,59 @@ function filterCities(univ, citiesSelect) {
     return arrUnivCities;
   }
 }
-function filterBranch(branch, theSelect) {
-  let branchSelect = document.querySelector(`#${theSelect} option:checked`);
+
+function filterUniversityAndCollege(theData, id, index) {
+  let arrResult = [];
+  let universitySelect = document.querySelector(`#${id} option:checked`).value;
+  if (universitySelect == "all") {
+    return theData;
+  } else {
+    theData.forEach((el) => {
+      let univSplit = el.university.split("/")[index];
+      if (universitySelect.trim() == univSplit.trim()) {
+        arrResult.push(el);
+      }
+    });
+    return arrResult;
+  }
+}
+
+function filterBranchAndGender(branch, id, type) {
+  let branchSelect = document.querySelector(`#${id} option:checked`);
   let arrBranch = [];
   if (branchSelect.value == "all") {
     return branch;
   } else {
     branch.forEach((el) => {
-      if (el[theSelect] == branchSelect.value || el[theSelect] == "مختلط") {
+      if (el[type] == branchSelect.value || el[type] == "مختلط") {
         arrBranch.push(el);
       }
     });
     return arrBranch;
+  }
+}
+function filterAverage(average) {
+  let averageSelect = document.querySelector(`#average`);
+  let arrAverage = [];
+  if (+averageSelect.value >= 50 && +averageSelect.value < 106) {
+    average.forEach((el) => {
+      if (el.average <= +averageSelect.value) {
+        arrAverage.push(el);
+      }
+    });
+    if (arrAverage.length >= 1) {
+      return arrAverage;
+    } else {
+      alert("لا توجد نتائج لهذه الاختيارات");
+      arrAverage.length = 0;
+      return arrAverage;
+    }
+  } else if (+averageSelect.value == 0) {
+    return average;
+  } else if (+averageSelect.value < 50 || +averageSelect.value > 105) {
+    alert("ادخل معدل صحيح");
+    arrAverage.length = 0;
+    return arrAverage;
   }
 }
 function appendResult(final, citiesSelect) {
@@ -157,22 +280,3 @@ function appendResult(final, citiesSelect) {
     tbody.appendChild(tr);
   });
 }
-// filterCities();
-// -------------- TEST -----------------  \\
-// let univ = new Set();
-// async function getAPI() {
-//   let getFetch = await fetch("./DB/DB2023.json");
-//   return await getFetch.json();
-// }
-// getAPI()
-//   .then((resolve) => {
-//     resolve.forEach((el) => {
-//       let univSplit = el.university.split("/")[0];
-//       univ.add(univSplit);
-//     });
-//     return univ;
-//   })
-//   .then((resolve) => {
-//     console.log(resolve);
-//   });
-// -------------- TEST -----------------  \\
